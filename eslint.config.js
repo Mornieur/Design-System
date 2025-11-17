42e9ba7a1a32a1c6e3e8c551beeabc039644660a
mornieur <mariafernanda-coro@hotmail.com>
2025-11-16 22:30:40 -0300

    chore(eslint): update flat config and clean up ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,23 +3,46 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default tseslint.config(
+const compat = new FlatCompat();
+
+export default [
+  // Arquivos ignorados
   {
-    ignores: ['.next/**', 'dist/**', 'build/**', 'coverage/**']
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'dist/**',
+      'build/**',
+      'coverage/**',
+      'out/**',
+      'next-env.d.ts'
+    ]
   },
 
-  {
+  // Config do Next.js (Converter do antigo para flat)
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  // Config do Storybook
+  ...storybook.configs['flat/recommended'],
+
+  // Config principal TS + React
+  tseslint.config({
     files: ['**/*.{ts,tsx}'],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
       parserOptions: {
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module'
-      }
+        project: true,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+      },
+      globals: globals.browser
     },
 
     plugins: {
@@ -30,16 +53,12 @@ export default tseslint.config(
     },
 
     settings: {
-      react: {
-        version: 'detect'
-      }
+      react: { version: 'detect' }
     },
 
     rules: {
       ...js.configs.recommended.rules,
-
       ...tseslint.configs.recommended[0].rules,
-
       ...reactHooks.configs.recommended.rules,
 
       'react-refresh/only-export-components': [
@@ -47,7 +66,8 @@ export default tseslint.config(
         { allowConstantExport: true }
       ],
 
+      // Prettier integrado
       'prettier/prettier': 'error'
     }
-  }
-);
+  })
+];
