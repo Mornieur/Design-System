83ea2fc768fe65dd57ec63b4354e54e96560703c
mornieur <mariafernanda-coro@hotmail.com>
2026-07-12 14:38:57 -0300

    fix: align next development toolchain

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
-import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/
-});
-
-const nextConfig: NextConfig = withMDX({
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-  webpack(config) {
-    return config;
+const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true
   }
-});
+};
 
 export default nextConfig;
