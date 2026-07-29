f278ebe8a0106f6732f85cfba7693cdfd2dcf0b5
mornieur <mariafernanda-coro@hotmail.com>
2026-07-29 15:12:54 -0300

    fix: use managed chromium in ci

--- playwright.visual.config.ts
+++ playwright.visual.config.ts
@@ -14,10 +14,6 @@ export default defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:3100',
     browserName: 'chromium',
-    launchOptions: {
-      executablePath:
-        'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
-    },
     locale: 'en-US',
     trace: 'off',
     video: 'off',
